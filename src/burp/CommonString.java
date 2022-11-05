a75bb9c8715de2589d5be613846fcdb74bf1912f
gand3lf <riccardo.cardelli1994@gmail.com>
2022-11-05 15:38:17 +0100

    Delete CommonString.java

--- src/burp/CommonString.java
+++
@@ -1,6 +0,0 @@
-package burp;
-
-public final class CommonString {
-    private static final String Author = "Riccardo Cardelli (@gand3lf)";
-    public static final String SEMDIR = "SEMDIR_BURP_PLUGIN";
-}
